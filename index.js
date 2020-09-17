1c6b2038c7b923a1c80cb40b462a2a2407ce0f19
dana <woworla821@gmail.com>
2020-09-18 00:13:04 +0900

    add babel

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+const app = express();
+const PORT = 3000;
+
+const handleListening = () =>
+	console.log(`Listening on: http://localhost:${PORT}`);
+const handleHome = (req, res) => res.send("Hello from my ass");
+const handleProfile = (req, res) => res.send("You are on my profile");
+
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
+app.listen(PORT, handleListening);
